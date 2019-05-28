5048a7af474fa8a9932dd151fc096aa71d714014
AndyPetes <32713958+AndyPetes@users.noreply.github.com>
2019-05-28 17:44:20 +0100

    Update 3Outgroup.py

--- 3Outgroup.py
+++ 3Outgroup.py
@@ -1,8 +1,9 @@
-#Usage:  python 3Outgroup.py Updated1001.txt AllThLyHaPe.txt EST.Loc.Thal.Lyr.Hall.Per.txt > AlleleInference.output
+#Usage:  python 3Outgroup.py Updated1001.txt AllThLyHaPe.txt EST.Loc.Thal.Lyr.Hall.Per.txt 3 > AlleleInference.output
 #Inpur Files:
 #sys.argv[1] = Updated1001.txt
 #sys.argv[2] = AllThLyHaPe.txt
 #sys.argv[3] = AllThLyHaPe.txt EST.Loc.Thal.Lyr.Hall.Per.txt
+#sys.argv[4] = Number of Outgroups (i.e. 3 or 2)
 
 
 #import sys
@@ -36,15 +37,30 @@ with open(sys.argv[2],"r") as IN:
 #Create output of Ancestral Alleles
 with open(sys.argv[3],"r") as IN:
         for line in IN:
-                fields = line.strip("\n").split(" ")
-                Outgroup1 = fields[-3]
-                Outgroup2 = fields[-2]
-                Outgroup3 = fields[-1]
-                Loc = fields[0]
-                if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0" and Outgroup3 == "0,0,0,0":
-                    print(Loc + "\t" + "NA")
-                Probability = Allele_Prob[Loc]
-                if float(Probability) >= 0.5:
-                        print(Loc + "\t" + Dic_Ref[Loc])
-                else:
-                        print(Loc + "\t" + Dic_Alt[Loc])
+                #Outgroups = 3
+                if sys.argv[4] == "3"
+                        fields = line.strip("\n").split(" ")
+                        Outgroup1 = fields[-3]
+                        Outgroup2 = fields[-2]
+                        Outgroup3 = fields[-1]
+                        Loc = fields[0]
+                        if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0" and Outgroup3 == "0,0,0,0":
+                                print(Loc + "\t" + "NA")
+                        Probability = Allele_Prob[Loc]
+                        if float(Probability) >= 0.5:
+                                print(Loc + "\t" + Dic_Ref[Loc])
+                        else:
+                                print(Loc + "\t" + Dic_Alt[Loc])
+                #Outgroups = 4 
+                if sys.argv[4] == "2"
+                        fields = line.strip("\n").split(" ")
+                        Outgroup1 = fields[-2]
+                        Outgroup2 = fields[-1]
+                        Loc = fields[0]
+                        if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0":
+                                print(Loc + "\t" + "NA")
+                        Probability = Allele_Prob[Loc]
+                        if float(Probability) >= 0.5:
+                                print(Loc + "\t" + Dic_Ref[Loc])
+                        else:
+                                print(Loc + "\t" + Dic_Alt[Loc])
