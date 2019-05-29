f051a0d7dd1687294a7aeea7144b8614f824299c
AndyPetes <32713958+AndyPetes@users.noreply.github.com>
2019-05-29 13:12:07 +0100

    Update Outgroup.py

--- Outgroup.py
+++ Outgroup.py
@@ -1,11 +1,11 @@
 #Usage:  python Outgroup.py Updated1001.txt AllThLyHaPe.txt EST.Loc.Thal.Lyr.Hall.Per.txt 3 > AlleleInference.output
+#Exact Usage: python Outgroup2.py ../../Thaliana/Updated1001.txt ../Pvalue/ThLyHaPe/AllLocThLyHaPe.txt ../EST.Loc.Thal.Lyr.Hall.Per.txt
+# 3 > ThLyHaPe.Allele.Output
 #Input Files:
 #sys.argv[1] = Updated1001.txt
 #sys.argv[2] = AllLocThLyHaPe.txt
 #sys.argv[3] = EST.Loc.Thal.Lyr.Hall.Per.txt
-#sys.argv[4] = Number of Outgroups (i.e. 3 or 2)
-
-
+#sys.argv[4] = Number of Outgroups (i.e. 2 or 3)
 import sys
 
 Dic_Ref = {}
@@ -14,55 +14,53 @@ Dic_Alt = {}
 
 #Create Dictionary from 1001.vcf file
 with open(sys.argv[1],"r") as IN:
-        for line in IN:
-                if line.startswith("#"):
-                        continue
-                fields = line.strip("\n").split(" ")
-                Chromosome = fields[0]
-                Position = str(fields[1])
-                Reference = fields[2]
-                Alternative = fields[3]
-                if len(Reference) == 1 and len(Alternative) == 1:
+       	for line in IN:
+               	if line.startswith("#"):
+                       	continue
+               	fields = line.strip("\n").split(" ")
+               	Chromosome = fields[0]
+               	Position = str(fields[1])
+               	Reference = fields[2]
+               	Alternative = fields[3]
+               	if len(Reference) == 1 and len(Alternative) == 1:
                     Dic_Ref[Chromosome + "_" + Position] = Reference
                     Dic_Alt[Chromosome + "_" + Position] = Alternative
-                    
-#Create dictopnary of the allele probailities
+
+#Create Dictionary of Locations with Probabilities that the allele is ancestral
 Allele_Prob = {}
 with open(sys.argv[2],"r") as IN:
-        for line in IN:
-                fields = line.strip("\n").split(" ")
-                Location = fields[0]
-                Probability = fields[8]
-                Allele_Prob[Location] = Probability
+       	for line in IN:
+               	fields = line.strip("\n").split(" ")
+               	new_fields = fields[0]
+               	Location = new_fields.split("\t")[0]
+               	Probability = fields[-6]
+               	Allele_Prob[Location] = Probability
 
-#Create output of Ancestral Alleles
+#Create Output for Ancestral Allele States
 with open(sys.argv[3],"r") as IN:
         for line in IN:
-                #Outgroups = 2 
-                if sys.argv[4] == "2"
-                        fields = line.strip("\n").split(" ")
-                        Outgroup1 = fields[-2]
-                        Outgroup2 = fields[-1]
-                        Loc = fields[0]
-                        if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0":
-                                print(Loc + "\t" + "NA")
-                        Probability = Allele_Prob[Loc]
-                        if float(Probability) >= 0.5:
-                                print(Loc + "\t" + Dic_Ref[Loc])
-                        else:
-                                print(Loc + "\t" + Dic_Alt[Loc])
-                #Outgroups = 3
-                if sys.argv[4] == "3"
-                        fields = line.strip("\n").split(" ")
-                        Outgroup1 = fields[-3]
-                        Outgroup2 = fields[-2]
-                        Outgroup3 = fields[-1]
-                        Loc = fields[0]
-                        if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0" and Outgroup3 == "0,0,0,0":
-                                print(Loc + "\t" + "NA")
-                        Probability = Allele_Prob[Loc]
-                        if float(Probability) >= 0.5:
-                                print(Loc + "\t" + Dic_Ref[Loc])
-                        else:
-                                print(Loc + "\t" + Dic_Alt[Loc])
-
+                if sys.argv[4] == "2":
+                     fields = line.strip("\n").split("\t")
+                     Outgroup1 = fields[-2]
+                     Outgroup2 = fields[-1]
+                     Loc = fields[0]
+                     Prob = Allele_Prob[Loc]
+                     if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0":
+                          print(Loc + "\t" + "NA")
+                     elif float(Prob) >= 0.5:
+                          print(Loc + "\t" + Dic_Ref[Loc] + "\t" + "Ancestral")
+                     else:
+                          print(Loc + "\t" + Dic_Alt[Loc] + "\t" + "Derived")
+                if sys.argv[4] == "3":
+                     fields = line.strip("\n").split("\t")
+                     Outgroup1 = fields[-3]
+                     Outgroup2 = fields[-2]
+                     Outgroup3 = fields[-1]
+                     Loc = fields[0]
+                     Prob = Allele_Prob[Loc]
+                     if Outgroup1 == "0,0,0,0" and Outgroup2 == "0,0,0,0" and Outgroup3 == "0,0,0,0":
+                          print(Loc + "\t" + "NA")
+                     elif float(Prob) >= 0.5:
+                          print(Loc + "\t" + Dic_Ref[Loc] + "\t" + "Ancestral")
+                     else:
+                          print(Loc + "\t" + Dic_Alt[Loc] + "\t" + "Derived")
